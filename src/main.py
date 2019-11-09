c11bb923b30c2f4054ec45a910e4b7d67a0762df
Yasuhiro <yasuhiro1026a@hotmail.com>
2019-11-09 23:21:04 +0900

    スクレイピング結果の表示対応

--- src/main.py
+++ src/main.py
@@ -16,11 +16,11 @@ def main():
     scrapingService = ScrapingService()
     files = glob.glob(os.path.join(args[1], '*.html'))
 
+    walkableProperties = []
     for aFile in files:
         # データ抽出
         properties = scrapingService.fetchFromHtmlFileAndScraping(aFile)
         for aPropaty in properties[:1]:
-            print(aPropaty.toString())
 
             # use google api
             google = GoogleAPI()
@@ -31,6 +31,18 @@ def main():
 
             # 時間をセット
             aPropaty.setTimeByWalk(receivedDict['rows'][0]['elements'][0]['duration']['value'])
+
+            # 距離をセット
+            aPropaty.setDistance(receivedDict['rows'][0]['elements'][0]['distance']['value'])
+
+            if aPropaty.canWalk() == True:
+                walkableProperties.append(aPropaty)
+
+    if len(walkableProperties) == 0:
+        print("There is nothing of walkable property")
+    else:
+        for aProperty in walkableProperties:
+            print(aProperty.toString())
 
 if __name__ == "__main__":
     main()
--- src/models/PropertyData.py
+++ src/models/PropertyData.py
@@ -5,6 +5,7 @@ class PropertyData:
     __name = ''
     __url = ''
     __timeByWalk = timedelta()
+    __distance = 0
 
     def __init__(
         self,
@@ -32,4 +33,13 @@ class PropertyData:
         self.__timeByWalk = timedelta(seconds=timeBySec)
 
     def toString(self):
-        return f"{self.__name}\n{self.__address}\n{self.__url}\n{self.__timeByWalk}\n"
+        return f"{self.__name}\n{self.__address}\n{self.__url}\n{self.__timeByWalk}\n{self.__distance}"
+
+    def distanceByKilloMeter(self):
+        self.__distance / 1000
+
+    def setDistance(self, distance):
+        self.__distance = distance
+
+    def canWalk(self):
+        return self.__distance < 2000
